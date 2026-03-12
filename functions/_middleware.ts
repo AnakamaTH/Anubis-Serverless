c74d554f898272224489144b7931584612a2af06
Anakama <contact@anakama.xyz>
2026-03-12 06:32:14 +0300

    Update _middleware.ts

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -11,8 +11,8 @@ const CHALLENGE_TTL = 5 * 60 * 1000;
 // UI Strings
 const STRINGS = {
   title: "Security Check",
-  heading: "Security Check",
-  description: "Please verify you are human.",
+  heading: "Making sure you are not a bot!",
+  description: "Please wait a moment while we ensure the security of your connection.",
   btn_start: "I am human",
   btn_calculating: "Calculating...",
   btn_verifying: "Verifying...",
